2c68dafb98bf56f3e2f86188b69cb5c77309fd7b
Lukáš Stankovič <lukasstankovic@gmail.com>
2016-07-20 15:34:09 +0200

    Ikony a úprava rozlišení pro 5s

--- ViewController.swift
+++ ViewController.swift
@@ -15,8 +15,15 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-        let htmlString:String! = "<iframe width='693' height='408' seamless frameborder='0' scrolling='no' src='https://docs.google.com/spreadsheets/d/1E-ElrybFLzFDbhbJryf5kbKQk6J29jDipateSb64Tvs/pubchart?oid=1100784856&amp;format=interactive'></iframe>"
+        // Do any additional setup after loading the view, typically from a nib
+        let htmlString:String! = "<iframe width='1100' height='708' seamless frameborder='0' scrolling='no' src='https://docs.google.com/spreadsheets/d/1E-ElrybFLzFDbhbJryf5kbKQk6J29jDipateSb64Tvs/pubchart?oid=1100784856&amp;format=interactive'></iframe>"
+
+       /* let url = NSURL (string: "http://meteopi.stankoviclukas.cz");
+        let requestObj = NSURLRequest(URL: url!);
+        webView.loadRequest(requestObj);*/
+        
+        self.webView.frame = self.view.bounds
+        self.webView.scalesPageToFit = true
         
         webView.loadHTMLString(htmlString, baseURL: nil)
         
