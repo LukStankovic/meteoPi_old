be8fb62dbb4098ecb76ec7b650e7e77bf5e0b419
Lukáš Stankovič <lukasstankovic@gmail.com>
2016-07-20 14:28:19 +0200

    Založení s web view

---
+++ ViewController.swift
@@ -0,0 +1,32 @@
+//
+//  ViewController.swift
+//  meteoPi
+//
+//  Created by Lukáš Stankovič on 20.07.16.
+//  Copyright © 2016 Lukáš Stankovič. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+
+    @IBOutlet var webView: UIWebView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        let htmlString:String! = "<iframe width='693' height='408' seamless frameborder='0' scrolling='no' src='https://docs.google.com/spreadsheets/d/1E-ElrybFLzFDbhbJryf5kbKQk6J29jDipateSb64Tvs/pubchart?oid=1100784856&amp;format=interactive'></iframe>"
+        
+        webView.loadHTMLString(htmlString, baseURL: nil)
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
